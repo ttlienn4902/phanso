4e75a44b4d3c0d25d14a16ba396a5139ea527c41
ttlienn4902 <101441307+ttlienn4902@users.noreply.github.com>
2022-03-13 20:39:22 +0700

    Add files via upload

---
+++ Programmang.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mang1chieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = new int();
+            int[] a=new int[n];
+            a[].nhap();
+        
+            a[].xuat();
+            a[].sapxep(x);
+            a[].timkiem(e);
+            Console.WriteLine(e);
+            Console.ReadLine(); 
+        }
+    }
+}
